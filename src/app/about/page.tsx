91531eec49b8ed1eac8c00b8faac637f1e4c42bd
Moiz Qureshi <m.moiz782008@gmail.com>
2025-05-13 13:42:48 +0500

    update
    push

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,6 +2,7 @@ import Feature from "@/components/Feature";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import Image from "next/image";
+import Moiz from "../../../public/images/post.jpeg"
 
 export default function AuthorCard (){
     return(
@@ -10,8 +11,8 @@ export default function AuthorCard (){
         <div className="bg-neutral-100 shadow-lg rounded-lg p-6 pt-12 pb-9">
             <div className="animation-fadeIn">
                 <div className="flex justify-center">
-            <Image src="/images/moiz.jpg" alt="Author Image" width={64} height={64} className="w-16 h-16 rounded-full mr-4 object-cover border-2 border-blue-500" />  
-            </div>
+                    <Image src={Moiz} alt="Author Image" width={64} height={64} className="w-16 h-16 rounded-full mr-4 object-cover border-2 border-blue-500" />  
+                </div>
                 <div>
                     <h3 className="text-xl font-bold text-center">Moiz Qureshi</h3>
                     <p className="text-slate-500 text-center">Web Developer | Graphic Designer | Web Designer</p>
@@ -22,7 +23,7 @@ export default function AuthorCard (){
             <div className="mt-4 flex space-x-3">
                 <a href="https://www.facebook.com/profile.php?id=100089981983705" className="px-4 py-2 text-white bg-blue-600 rounded-md hover:bg-blue-700 transition duration-300">Facebook</a>
                 <a href="https://github.com/MoizQureshi1" className="px-4 py-2 text-white bg-blue-600 rounded-md hover:bg-blue-700 transition duration-300">GitHubt</a>
-                <a href="https://www.youtube.com/@solve-swift" className="px-4 py-2 text-white bg-blue-600 rounded-md hover:bg-blue-700 transition duration-300">YouTube</a>
+                <a href="https://www.youtube.com/@Moiz_Qureshi00" className="px-4 py-2 text-white bg-blue-600 rounded-md hover:bg-blue-700 transition duration-300">YouTube</a>
             </div>
             </div>
         </div>
--- src/components/AuthoreCard.tsx
+++ src/components/AuthoreCard.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import Moiz from "../public/images/moiz.JPG";
+import Moiz from "../../public/images/post.jpeg";
 
 
 
@@ -7,7 +7,7 @@ export default function AuthorCard (){
     return(
         <div className="bg-white shadow-lg rounded-lg p-6 mt-12">
             <div className="flex items-center animation-fadeIn">
-            <Image src="Moiz" alt="Author Image" width={64} height={64} className="w-16 h-16 rounded-full mr-4 object-cover border-2 border-blue-500" />  
+            <Image src={Moiz} alt="Author Image" width={64} height={64} className="w-16 h-16 rounded-full mr-4 object-cover border-2 border-blue-500" />  
                 <div>
                     <h3 className="text-xl font-bold">Moiz Qureshi</h3>
                     <p className="text-slate-500">Web Developer | Graphic Designer | Web Designer</p>
@@ -17,7 +17,7 @@ export default function AuthorCard (){
             <div className="mt-4 flex space-x-3">
                 <a href="https://www.facebook.com/profile.php?id=100089981983705" className="px-4 py-2 text-white bg-blue-600 rounded-md hover:bg-blue-700 transition duration-300">Facebook</a>
                 <a href="https://github.com/MoizQureshi1" className="px-4 py-2 text-white bg-blue-600 rounded-md hover:bg-blue-700 transition duration-300">GitHubt</a>
-                <a href="https://www.youtube.com/@solve-swift" className="px-4 py-2 text-white bg-blue-600 rounded-md hover:bg-blue-700 transition duration-300">YouTube</a>
+                <a href="https://www.youtube.com/@Moiz_Qureshi00" className="px-4 py-2 text-white bg-blue-600 rounded-md hover:bg-blue-700 transition duration-300">YouTube</a>
             </div>
         </div>
     )
